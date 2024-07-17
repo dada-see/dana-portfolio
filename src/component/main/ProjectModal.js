a3a7fb4ceb9b9149c0e6b35bb06470921909bc09
dada-see <dleksk12@naver.com>
2024-07-17 16:00:36 +0900

    모달창 구조 및 모바일 css

--- src/component/main/ProjectModal.js
+++ src/component/main/ProjectModal.js
@@ -1,33 +1,96 @@
 const ProjectModal = ({project, openModal, handleModal}) => {
     return(
         <div className={`ProjectModal ${project.title}`}>
-            <div className="side_bar">
-                <div className="round">
-                    <a href={project.url[0]} target='_blank' rel="noreferrer" >
-                        <svg width="28" height="28" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 98 96">
-                            <path fillRule="evenodd" clipRule="evenodd" d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z" fill="#24292f"/>
-                        </svg>
-                        <span className="pc">GitHub</span>
-                    </a>
-                </div>
-                <p className="icon">
-                    <a href={project.url[1]} target="_blank" rel="noreferrer">
-                        <span className="material-symbols-outlined">link</span>
-                        <span className="pc">배포 링크</span>
-                    </a>
-                </p>
+            <nav className="side_bar">
+                <ul>
+                    <li className="round">
+                        <a href={project.url[0]} target='_blank' rel="noreferrer" >
+                            <svg width="20" height="20" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 98 96">
+                                <path fillRule="evenodd" clipRule="evenodd" d="M48.854 0C21.839 0 0 22 0 49.217c0 21.756 13.993 40.172 33.405 46.69 2.427.49 3.316-1.059 3.316-2.362 0-1.141-.08-5.052-.08-9.127-13.59 2.934-16.42-5.867-16.42-5.867-2.184-5.704-5.42-7.17-5.42-7.17-4.448-3.015.324-3.015.324-3.015 4.934.326 7.523 5.052 7.523 5.052 4.367 7.496 11.404 5.378 14.235 4.074.404-3.178 1.699-5.378 3.074-6.6-10.839-1.141-22.243-5.378-22.243-24.283 0-5.378 1.94-9.778 5.014-13.2-.485-1.222-2.184-6.275.486-13.038 0 0 4.125-1.304 13.426 5.052a46.97 46.97 0 0 1 12.214-1.63c4.125 0 8.33.571 12.213 1.63 9.302-6.356 13.427-5.052 13.427-5.052 2.67 6.763.97 11.816.485 13.038 3.155 3.422 5.015 7.822 5.015 13.2 0 18.905-11.404 23.06-22.324 24.283 1.78 1.548 3.316 4.481 3.316 9.126 0 6.6-.08 11.897-.08 13.526 0 1.304.89 2.853 3.316 2.364 19.412-6.52 33.405-24.935 33.405-46.691C97.707 22 75.788 0 48.854 0z" fill="#24292f"/>
+                            </svg>
+                            <span className="pc">GitHub</span>
+                        </a>
+                    </li>
+                    <li className="icon">
+                        <a href={project.url[1]} target="_blank" rel="noreferrer">
+                            <span className="material-symbols-outlined">link</span>
+                            <span className="pc">배포 링크</span>
+                        </a>
+                    </li>
+                </ul>
                 <button onClick={()=>handleModal()}>
                     <span className="material-symbols-outlined">close</span>
                 </button>
-            </div>
-            <div className="main_info">
-                <div className="feature">
-                    {project.feature.map((feature, index)=>
-                        <span key={index}>{feature}</span>
-                    )}
-                    <h4>{project.title}</h4>
-                    
-                </div>
+            </nav>
+            <div className="wrap">
+                <section className="main_info">
+                    <div className="main_info_wrap">
+                        <div className="feature">
+                            {project.feature.map((feature, index)=>
+                                <span key={index}>{feature}</span>
+                            )}
+                        </div>
+                        <h4>{project.title}</h4>
+                        <div className="usedSkill">
+                            {project.useSkill.map((skill, index)=>
+                                <span key={index}>#{skill} </span>
+                            )}
+                        </div>
+                        <div className="main_photo">
+                            <img src={`./img/projects/${project.img[0]}.jpg`} alt="프로젝트 이미지"/>
+                        </div>
+                        <p className="intro">{project.intro}</p>
+                    </div>
+                </section>
+                <section className="text">
+                    <article className="function">
+                        <h5>📍 주요 기능 및 특징</h5>
+                        <ul>
+                            {project.function.map((func, index)=>
+                                <li key={index}>{func}</li>
+                            )}
+                        </ul>
+                    </article>
+                    <article className="skill">
+                        <h5>🛠️ 사용 기술 및 언어</h5>
+                        <ul>
+                            {project.useSkill.map((skill, index)=>
+                                <li key={index}>{skill}</li>
+                            )}
+                        </ul>
+                    </article>
+                    <article className="whatIDid">
+                        <h5>🌟 작업 기여도</h5>
+                        <ul>
+                            {project.whatIDid.map((what, index)=>(
+                                <li key={index}>
+                                    <h6>{what.title}</h6>
+                                    {what.details.map((detail, index)=>
+                                        <p key={index}>{detail}</p>
+                                    )}                            
+                                </li>
+                            ))}
+                        </ul>
+                    </article>
+                    <article className="trouble">
+                        <h5>🎯 Trouble Shooting</h5>
+                        <ul>
+                            {project.trouble.map((troub, index)=>
+                                <li key={index}>
+                                    <h6>{troub.name}</h6>
+                                    <p>
+                                        <span className="spot">[문제점]&nbsp;</span>
+                                        {troub.issue}
+                                    </p>
+                                    <p>
+                                        <span className="spot">[해결]&nbsp;</span>
+                                        {troub.solve}
+                                    </p>
+                                </li>
+                            )}
+                        </ul>
+                    </article>
+                </section>
             </div>
         </div>
     )
